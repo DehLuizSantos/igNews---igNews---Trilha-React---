ce3ecf25496ff6bd4f7ff661b135a90be2a385bf
DehLuizSantos <and_consul@outlook.com>
2022-05-09 22:02:44 -0300

    feature(home)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from "next/app";
+import { Header } from "../components/Header";
 import "../styles/global.scss";
 
 // Pagina que fica em volta de tudo (é reexecutado sempre que o usuário trocar de tala)
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+
+    </>
+  )
 }
 
 export default MyApp;
